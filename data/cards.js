ac2abad7583e4e4cce69edcbcc900760e86c9842
its-slept <noobyt143op@gmail.com>
2026-05-20 09:23:04 -0500

    add card 1165: Jinshi #1

--- data/cards.js
+++ data/cards.js
@@ -9338,5 +9338,13 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Skirk-1.webp"
+},{
+  "id": "1165",
+  "name": "Jinshi #1",
+  "series": "Wuthering Waves",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Jinshi-1.webp"
 }
 ];
