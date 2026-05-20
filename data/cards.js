cea6aab20acfb2d47bfe66e673762cf7ed1f0d56
its-slept <noobyt143op@gmail.com>
2026-05-20 11:28:34 -0500

    add card 1185: Evil Neuro #1

--- data/cards.js
+++ data/cards.js
@@ -9498,5 +9498,13 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Shuna-1.webp"
+},{
+  "id": "1185",
+  "name": "Evil Neuro #1",
+  "series": "Vtubers & Youtubers",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Evil-Neuro-1.webp"
 }
 ];
