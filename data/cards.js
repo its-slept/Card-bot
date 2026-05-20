b31fe28e3e3fb6379853126f4dd1bae3766d536c
its-slept <noobyt143op@gmail.com>
2026-05-20 13:07:37 -0500

    add card 1208: Nozel Silva #1

--- data/cards.js
+++ data/cards.js
@@ -9682,5 +9682,13 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Dorothy-Unsworth-1.webp"
+},{
+  "id": "1208",
+  "name": "Nozel Silva #1",
+  "series": "Black Clover",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Nozel-Silva-1.webp"
 }
 ];
