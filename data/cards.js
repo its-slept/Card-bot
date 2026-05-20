ad1931990af879049789967f9bb95fb98757b3c4
its-slept <noobyt143op@gmail.com>
2026-05-20 13:31:30 -0500

    add card 1218: Murakumo Kagetsu #1

--- data/cards.js
+++ data/cards.js
@@ -9762,5 +9762,13 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-9/Alban-Knox-1.webp"
+},{
+  "id": "1218",
+  "name": "Murakumo Kagetsu #1",
+  "series": "Nijisanji",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-9/Murakumo-Kagetsu-1.webp"
 }
 ];
