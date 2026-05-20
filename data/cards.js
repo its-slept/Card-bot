adecf34e6a0f884c7c42de52010cb19a5c1f0ba2
its-slept <noobyt143op@gmail.com>
2026-05-20 07:26:03 -0500

    add card 1159: Nami #3

--- data/cards.js
+++ data/cards.js
@@ -9290,5 +9290,13 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/v1.0/Permanent-7/Takamiya-Mio-1.webp"
+},{
+  "id": "1159",
+  "name": "Nami #3",
+  "series": "One Piece",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Nami-#3.webp"
 }
 ];
