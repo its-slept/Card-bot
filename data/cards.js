21e607fbcad75e86674fb9949c07b8a6af9ed823
its-slept <noobyt143op@gmail.com>
2026-05-20 13:12:00 -0500

    add card 1210: Barbara #1

--- data/cards.js
+++ data/cards.js
@@ -9698,5 +9698,13 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-9/William-Vangeance-1.webp"
+},{
+  "id": "1210",
+  "name": "Barbara #1",
+  "series": "Genshin Impact",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-9/Barbara-1.webp"
 }
 ];
