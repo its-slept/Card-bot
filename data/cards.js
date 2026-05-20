33cb4575b952af564f500ec34ee91935164c58fb
its-slept <noobyt143op@gmail.com>
2026-05-20 14:04:50 -0500

    add card 1228: Cabernet

--- data/cards.js
+++ data/cards.js
@@ -9842,5 +9842,13 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-9/Angell-1.webp"
+},{
+  "id": "1228",
+  "name": "Cabernet",
+  "series": "Path To Nowhere",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-9/Cabernet.webp"
 }
 ];
