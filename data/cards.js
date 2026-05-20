bbd0e01dcbe1533ea4466e340b2d6740e7ddff98
its-slept <noobyt143op@gmail.com>
2026-05-20 11:38:18 -0500

    add card 1193: Mint Fantome

--- data/cards.js
+++ data/cards.js
@@ -9562,5 +9562,13 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Aicandii-1.webp"
+},{
+  "id": "1193",
+  "name": "Mint Fantome",
+  "series": "Vtubers & Youtubers",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-8/Mint-Fantome.webp"
 }
 ];
